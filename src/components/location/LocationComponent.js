7bc355be6b3a9e348358af2f65964a26c0448936
Ana_stasya_S <anastasiya-s@live.de>
2020-04-28 11:47:40 +0300

    fix: render map

--- src/components/location/LocationComponent.js
+++ src/components/location/LocationComponent.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { YMaps, Map } from 'react-yandex-maps';
 
 const LocationComponent = ({ coordinate }) => {
-  if (!coordinate) {
+  if (!coordinate.length) {
     return (
       <div>
         I don't know, where you are
       </div>
     )
   } 
-  if(coordinate) {
+  if(coordinate.length) {
     return (
       <div>
         <YMaps>
